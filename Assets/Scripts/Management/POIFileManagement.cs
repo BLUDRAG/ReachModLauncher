db4649f14f7723e9c74dcb60a574a3ed4b1dab45
BLUDRAG <kearan.petersen@gmail.com>
2023-05-21 19:44:38 +0800

    Reformatted the POI File Management script.

--- Assets/Scripts/Management/POIFileManagement.cs
+++ Assets/Scripts/Management/POIFileManagement.cs
@@ -4,35 +4,33 @@ using System.Linq;
 
 namespace ReachModLauncher
 {
-    public static class POIFileManagement
-    {
-        private static readonly (string type, string file, bool found)[] _requiredPOIFiles = new (string, string, bool)[]
-                                                                                             {
-	                                                                                             (".nim", null, false),
-	                                                                                             (".ins", null, false),
-	                                                                                             (".tts", null, false),
-	                                                                                             (".xml", null, false),
-                                                                                             };
-        
-        private static readonly (string type, string file, bool found)[] _optionalPOIFiles = new (string, string, bool)[]
-                                                                                             {
-	                                                                                             (".mesh", null, false),
-	                                                                                             (".jpg", null, false),
-                                                                                             };
+	public static class POIFileManagement
+	{
+		private static readonly (string type, string file, bool found)[] _requiredPOIFiles =
+			new (string, string, bool)[]
+			{
+				(".nim", null, false), (".ins", null, false), (".tts", null, false), (".xml", null, false),
+			};
 
-        public static void CapturePOIFiles(string directory)
-        {
-	        ResetPOIFiles();
-	        string[] files = Directory.GetFiles(directory);
-        	
-	        foreach(string file in files)
-	        {
-		        UpdatePOIList(_requiredPOIFiles, file);
-		        UpdatePOIList(_optionalPOIFiles, file);
-	        }
-        }
-        
-        public static List<string> GetMissingPOIFiles()
+		private static readonly (string type, string file, bool found)[] _optionalPOIFiles =
+			new (string, string, bool)[]
+			{
+				(".mesh", null, false), (".jpg", null, false),
+			};
+
+		public static void CapturePOIFiles(string directory)
+		{
+			ResetPOIFiles();
+			string[] files = Directory.GetFiles(directory);
+
+			foreach(string file in files)
+			{
+				UpdatePOIList(_requiredPOIFiles, file);
+				UpdatePOIList(_optionalPOIFiles, file);
+			}
+		}
+
+		public static List<string> GetMissingPOIFiles()
 		{
 			List<string> missingFiles = new List<string>();
 
@@ -43,50 +41,50 @@ namespace ReachModLauncher
 					missingFiles.Add(data.type);
 				}
 			}
-			
+
 			return missingFiles;
 		}
-        
-        public static List<string> GetPOIFiles()
+
+		public static List<string> GetPOIFiles()
 		{
-	        List<string> poiFiles = new List<string>();
-	        
-	        poiFiles.AddRange(_requiredPOIFiles.Select(data => data.file).ToList());
-	        poiFiles.AddRange(_optionalPOIFiles.Select(data => data.file).ToList());
-	        
-	        return poiFiles;
+			List<string> poiFiles = new List<string>();
+
+			poiFiles.AddRange(_requiredPOIFiles.Select(data => data.file).ToList());
+			poiFiles.AddRange(_optionalPOIFiles.Select(data => data.file).ToList());
+
+			return poiFiles;
 		}
 
-        private static void ResetPOIFiles()
-        {
-        	for(int i = 0; i < _requiredPOIFiles.Length; i++)
-        	{
-        		(string type, string file, bool found) requiredPOIFile = _requiredPOIFiles[i];
-        		requiredPOIFile.file  = null;
-        		requiredPOIFile.found = false;
-        		_requiredPOIFiles[i]  = requiredPOIFile;
-        	}
+		private static void ResetPOIFiles()
+		{
+			for(int i = 0; i < _requiredPOIFiles.Length; i++)
+			{
+				(string type, string file, bool found) requiredPOIFile = _requiredPOIFiles[i];
+				requiredPOIFile.file  = null;
+				requiredPOIFile.found = false;
+				_requiredPOIFiles[i]  = requiredPOIFile;
+			}
 
-        	for(int i = 0; i < _optionalPOIFiles.Length; i++)
-        	{
-        		(string type, string file, bool found) optionalPOIFile = _optionalPOIFiles[i];
-        		optionalPOIFile.file  = null;
-        		optionalPOIFile.found = false;
-        		_optionalPOIFiles[i]  = optionalPOIFile;
-        	}
-        }
+			for(int i = 0; i < _optionalPOIFiles.Length; i++)
+			{
+				(string type, string file, bool found) optionalPOIFile = _optionalPOIFiles[i];
+				optionalPOIFile.file  = null;
+				optionalPOIFile.found = false;
+				_optionalPOIFiles[i]  = optionalPOIFile;
+			}
+		}
 
-        private static void UpdatePOIList((string type, string file, bool found)[] list, string file)
-        {
-        	string extension = Path.GetExtension(file);
-        	
-        	for(int i = 0; i < list.Length; i++)
-        	{
-        		if(list[i].type != extension) continue;
+		private static void UpdatePOIList((string type, string file, bool found)[] list, string file)
+		{
+			string extension = Path.GetExtension(file);
 
-        		list[i].file  = file;
-        		list[i].found = true;
-        	}
-        }
-    }
+			for(int i = 0; i < list.Length; i++)
+			{
+				if(list[i].type != extension) continue;
+
+				list[i].file  = file;
+				list[i].found = true;
+			}
+		}
+	}
 }
