abc21fde1e3c2f3803d131ba74302aae1da9bf3f
BLUDRAG <kearan.petersen@gmail.com>
2023-05-29 07:15:48 +0800

    Added support for retrieving the list of available user files and downloading files from Google Drive.

--- Assets/Scripts/Management/GoogleDriveManagement.cs
+++ Assets/Scripts/Management/GoogleDriveManagement.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
 using Google.Apis.Drive.v3;
 using UnityEngine;
 using File = Google.Apis.Drive.v3.Data.File;
@@ -38,8 +41,9 @@ namespace ReachModLauncher
 			fileRequest.Fields = "*";
 			fileRequest.Q      = $"'me' in owners and '{folder}' in parents and name = '{file}'";
 			IList<File> files = (await fileRequest.ExecuteAsync()).Files;
+			bool hasFiles = files.Count > 0;
 
-			return (files.Count > 0, files.Count > 0 ? files[0].Id : null);
+			return (hasFiles, hasFiles ? files[0].Id : null);
 		}
 
 		public static async Task<(bool exists, string id)> FolderExists(string folder)
@@ -66,13 +70,19 @@ namespace ReachModLauncher
 			return result.Id;
 		}
 
-		public static async Task<IList<File>> GetFiles(string folder)
+		public static async Task<List<File>> GetFiles(string folder)
 		{
 			FilesResource.ListRequest fileRequest = _service.Files.List();
-			fileRequest.Q = $"'me' in owners and '{folder}' in parents";
+			fileRequest.Fields = "*";
+			fileRequest.Q      = $"'me' in owners and '{folder}' in parents";
 			IList<File> files = (await fileRequest.ExecuteAsync()).Files;
 
-			return files;
+			return files.ToList();
+		}
+
+		public static async Task<List<string>> GetUsers()
+		{
+			return (await GetFiles(_repoDirectory)).Select(file => file.Id).ToList();
 		}
 
 		public static async Task UploadFile(string folder, string filename, Stream stream)
@@ -97,6 +107,23 @@ namespace ReachModLauncher
 			{
 				await stream.DisposeAsync();
 			}
+		}
+		
+		public static async Task<byte[]> DownloadFile(File file, Action<long> progressCallback)
+		{
+			FilesResource.GetRequest request = _service.Files.Get(file.Id);
+			request.Fields = "*";
+			long                    totalBytes = file.Size ?? long.MaxValue;
+			MemoryStream            stream     = new MemoryStream();
+			Task<IDownloadProgress> progress   = request.DownloadAsync(stream);
+			
+			while(!progress.IsCompleted)
+			{
+				progressCallback?.Invoke(progress.Result.BytesDownloaded / totalBytes);
+				await Task.Yield();
+			}
+			
+			return stream.ToArray();
 		}
 	}
 }
