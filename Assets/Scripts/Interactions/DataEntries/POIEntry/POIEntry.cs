d2e24060aeaab23fc36e056ed5856fd7b369e875
BLUDRAG <kearan.petersen@gmail.com>
2023-05-29 07:26:41 +0800

    Added the required scene references to the POI Entry component and applied the associated references within the Main scene.

--- Assets/Scripts/Interactions/DataEntries/POIEntry/POIEntry.cs
+++ Assets/Scripts/Interactions/DataEntries/POIEntry/POIEntry.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ReachModLauncher
 {
     public class POIEntry : MonoBehaviour
     {
-        
+        public POIData  Data;
+        public TextLink Link;
+        public Image    Preview;
     }
 }
