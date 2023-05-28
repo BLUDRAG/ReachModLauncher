f3a46e16e85f8616820b347770268a95cc81c5c6
BLUDRAG <kearan.petersen@gmail.com>
2023-05-29 07:26:12 +0800

    Abstracted the Text Link logic to a dedicated script and updated the Mod Link component accordingly.

--- Assets/Scripts/Interactions/DataEntries/ModEntry/ModLink.cs
+++ Assets/Scripts/Interactions/DataEntries/ModEntry/ModLink.cs
@@ -1,20 +1,10 @@
 using System.Collections.Generic;
-using System.Diagnostics;
-using TMPro;
-using UnityEngine;
 using UnityEngine.UI.Extensions;
 
 namespace ReachModLauncher
 {
-	public class ModLink : MonoBehaviour
+	public class ModLink : TextLink
 	{
-		public string                            Link;
-		public TMP_Text                          Text;
 		public List<BoundTooltipTriggerExtended> Tooltips = new List<BoundTooltipTriggerExtended>();
-
-		public void OnLinkClicked()
-		{
-			Process.Start(Link);
-		}
 	}
 }
---
+++ Assets/Scripts/Interactions/TextLink.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using TMPro;
+using UnityEngine;
+
+namespace ReachModLauncher
+{
+    public class TextLink : MonoBehaviour
+    {
+        public string   Link;
+        public TMP_Text Text;
+
+        public void OnLinkClicked()
+        {
+            Process.Start(Link);
+        }
+    }
+}
